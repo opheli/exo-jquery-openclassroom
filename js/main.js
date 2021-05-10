c96470857a7049e50937cdaf7aa63b7b69b66c92
ophélie <ophelieleleu@yahoo.fr>
2021-05-10 13:26:39 +0200

    fin exo

--- js/main.js
+++ js/main.js
@@ -9,20 +9,68 @@ $(document).ready(function () {
         $erreur = $('#erreur'),
         $champ = $('.champ');
 
-    $champ.keyup(function () {
-        if ($(this).val().length < 5) {
-            $(this).css({
-                borderColor: 'red',
-                color: 'red'
-            });
-        }
-        else {
-            $(this).css({
-                borderColor: 'green',
-                color: 'green'
-            });
+    $pseudo.keyup(function () {
+        console.log($pseudo.val().length > 5);
+        if ($pseudo.val().length > 5) {
+            $pseudo.removeClass("red");
+            $pseudo.addClass("green");
+        } else {
+            $pseudo.removeClass("green");
+            $pseudo.addClass("red");
         }
     });
+
+    $mdp.keyup(function () {
+        console.log($mdp.val().length > 5);
+        if ($mdp.val().length > 5) {
+            $mdp.removeClass("red");
+            $mdp.addClass("green");
+        } else {
+            $mdp.removeClass("green");
+            $mdp.addClass("red");
+        }
+    });
+
+    $confirmation.keyup(function () {
+        //if ($confirmation.val() === $mdp.val()) {
+        if ($confirmation.val() === $mdp.val()) {
+            $confirmation.removeClass("red");
+            $confirmation.addClass("green");
+        } else {
+            $confirmation.removeClass("green");
+            $confirmation.addClass("red");
+        }
+    });
+
+    function verifier(champ){
+        if(champ.val() == ""){ // si le champ est vide
+            $erreur.css('display', 'block'); // on affiche le message d'erreur
+            champ.css({ // on rend le champ rouge
+                borderColor : 'red',
+                color : 'red'
+            });
+        }
+    }
+
+    $envoi.click(function(e){
+        e.preventDefault(); // on annule la fonction par défaut du bouton d'envoi
+    
+        // puis on lance la fonction de vérification sur tous les champs :
+        verifier($pseudo);
+        verifier($mdp);
+        verifier($confirmation);
+        verifier($mail);
+    });
+    
+
+    // $('#envoi').click(function () {
+       
+    //     if ($pseudo === true || $mdp === true || $confirmation === true) {
+    //         $('#envoi').addClass("greenTwo");
+    //     } else {
+    //         $('#envoi').addClass("redTwo");
+    //     }
+    // });
 
 });
 
